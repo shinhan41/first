785bbc143b70a560cf673beb9be8ef6ea5a21d49
xodlf100 <xodlf100@gmail.com>
2023-04-26 15:16:04 +0900

    ㅇ

---
+++ src/main/java/first/folio1/Folio1Application.java
@@ -0,0 +1,12 @@
+package first.folio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Folio1Application{
+
+    public static void main(String[] args){
+        SpringApplication.run(Folio1Application.class,args);
+    }
+}
---
+++ src/test/java/first/folio1/Folio1ApplicationTests.java
@@ -0,0 +1,12 @@
+package first.folio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Folio1ApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+}
